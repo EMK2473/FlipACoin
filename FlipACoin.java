3f196d8aec396bd4154e2f42171aa5d962185392
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-10 14:47:13 -0400

    defined coinFlip method; declared and assigned result variable ot next rand int; conditional output for heads or tails

--- FlipACoin.java
+++ FlipACoin.java
@@ -5,7 +5,16 @@ import java.util.Random;
 
 public class FlipACoin {
    
-   // TODO: Define your method here
+   public static String coinFlip(Random rand) {
+    int result = rand.nextInt(2);
+    if(result == 1){
+        return "Heads";
+    }
+    else {
+        return "Tails";
+    }
+
+   }
    
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
