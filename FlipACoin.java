66a5b480af3d8274084b94f46bf0c93e9b90e9bb
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-10 14:34:58 -0400

    init file

---
+++ FlipACoin.java
@@ -0,0 +1,16 @@
+package FlipACoin;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class FlipACoin {
+   
+   // TODO: Define your method here
+   
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      Random rand = new Random(2); // Unique seed
+      
+      // TODO: Complete main here
+   }
+}
